60e2f199b0c1026e5815ca397b05df9deec2b734
LakeCombs <adesolaolamilekan2@gmailcom>
2021-07-02 12:41:07 +0100

    added private routes

---
+++ backend/controllers/private.js
@@ -0,0 +1,8 @@
+exports.getPrivateData = (req, res, next) => {
+  res
+    .status(200)
+    .json({
+      succes: true,
+      data: "you got access to private data in this route",
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const cors = require("cors");
-const authroutes = require("./routes/auth");
+const authRoutes = require("./routes/auth");
+const privateRoutes = require("./routes/private");
 const connectDB = require("./config/db");
 const errorHandler = require("./middlewares/error");
 
@@ -11,7 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors);
-app.use("/api/auth", authroutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/private", privateRoutes);
 
 //Error handlers
 app.use(errorHandler);
